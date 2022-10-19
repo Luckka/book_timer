f2bdfc282eeab8712cf49bc2f196f22afe238b9c
Lucka <lucka.dev.pj@dippingcode.com.br>
2022-10-19 15:54:30 -0300

    feat: add splash page

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,3 +1,4 @@
+import 'package:book_timer/app/core/ui/font_styles.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
@@ -14,8 +15,26 @@ class SplashPage extends StatelessWidget {
                 Color(0XFF22234D)
               ])
              ),
-             child: Center(
-              child: Image.asset('assets/images/logo_book.png'),
+             child: Padding(
+               padding: const EdgeInsets.only(top:100.0),
+               child: Column(
+                 children: [
+                   Image.asset('assets/images/logo_book.png'),
+                   const SizedBox(height: 10,),
+                   Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                     children: [
+                       SizedBox(
+                        height: 30,
+                        width: 30,
+                        child: Image.asset('assets/images/reading.png')
+                      ),
+                      const SizedBox(width: 5,),
+                       Text('Book Timer',style: FontStyles().headingMedium),
+                     ],
+                   )
+                 ],
+               ),
              ),
            ),
        );
